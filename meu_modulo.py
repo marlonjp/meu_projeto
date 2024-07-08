02658fcd9065c08a60b7a5855952dec94762bc55
Marlon Pinheiro <marlon.pinheiro.externo@ageas.pt>
2024-07-09 00:30:37 +0100

    Primeiro Commit

---
+++ meu_modulo.py
@@ -0,0 +1,16 @@
+def soma(a, b):
+    return a + b
+
+# def multiplica(a, b):
+#     return a * b
+
+# def opera(a, b, operacao):
+#     if operacao == 'soma':
+#         return soma(a, b)
+#     elif operacao == 'multiplica':
+#         return multiplica(a, b)
+#     else:
+#         return None
+
+resultado = soma(2,2)
+print("O resultado da soma é: ", str(resultado))
